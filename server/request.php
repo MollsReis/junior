96f30ed0781beb38945f825957c23c3115a20feb
Scott Reis <scott.reis@vertive.com>
2011-07-18 13:46:08 -0500

    create junior server request and server classes

---
+++ server/request.php
@@ -0,0 +1,105 @@
+<?php
+namespace Junior\Server;
+
+const JSON_RPC_VERSION = "2.0";
+const ERROR_PARSE_ERROR = -32700;
+const ERROR_INVALID_REQUEST = -32600;
+const ERROR_MISMATCHED_VERSION = -32000;
+
+class Request {
+
+    // create new server request object from raw json
+    public function __construct($json)
+    {
+        $this->raw = $json;
+        $obj = json_decode($json);
+
+        // handle json parse error
+        if ($obj === null) {
+            $this->error_code = ERROR_PARSE_ERROR;
+            $this->error_message = "Parse error.";
+            return;
+        }
+
+        // array of objects for batch
+        if (is_array($obj)) {
+            $this->batch = true;
+            $this->requests = array();
+            foreach ($obj as $req) {
+                $this->requests[] = new Request(json_encode($req));
+            }
+        // single request
+        } else {
+            $this->batch = false;
+            $this->json_rpc = $obj->jsonrpc;
+            $this->method = $obj->method;
+            $this->params = $obj->params;
+            $this->id = $obj->id;
+        }
+    }
+
+    // returns true if request is valid or returns false assigns error
+    public function checkValid()
+    {
+        // mismatched json-rpc version
+        if ($this->json_rpc != "2.0") {
+            $this->error_code = ERROR_MISMATCHED_VERSION;
+            $this->error_version = "Client/Server JSON-RPC version mismatch.";
+            return false;
+        }
+
+        // illegal method name
+        if (!is_string($this->method)) {
+            $this->error_code = ERROR_INVALID_REQUEST;
+            $this->error_message = "Invalid request.";
+            return false;
+        }
+
+        // valid request
+        return true;
+    }
+
+    // returns true if request is a batch
+    public function isBatch()
+    {
+        return $this->batch;
+    }
+
+    // returns true if request is a notification
+    public function isNotification()
+    {
+        if ($this->id) {
+            return false;
+        }
+        return true;
+    }
+
+    // return raw JSON response
+    public function toResponseJSON()
+    {
+        return json_encode($this->toArray());
+    }
+
+    // return assoc array for JSON response
+    public function toArray()
+    {
+        // successful response
+        $arr = array('jsonrpc' => JSON_RPC_VERSION);
+        if ($this->result) {
+            $arr['result'] = $this->result;
+            $arr['id'] = $this->id;
+            return $arr;
+        // error response
+        } else {
+            $arr['error'] = array('code' => $this->error_code, 'message' => $this->error_message);
+            if ($this->id) {
+                $arr['id'] = $this->id;
+            }
+            return $arr;
+        }
+    }
+
+}
+
+?>
+
--- server/server.php
+++ server/server.php
@@ -1,5 +1,88 @@
 <?php
+namespace Junior\Server;
 
-// TODO write the server
+const ERROR_METHOD_NOT_FOUND = -32601;
+const ERROR_INVALID_PARAMS = -32602;
+
+class Server {
+
+    // create new server
+    public function __construct($exposed_instance)
+    {
+        $this->exposed_instance = $exposed_instance;
+    }
+
+    // check for method existence
+    public function methodExists($method_name)
+    {
+        $reflection = new ReflectionClass($this->exposed_instace);
+        return $reflection->hasMethod($method_name);
+    }
+
+    // attempt to invoke the method with params
+    public function invokeMethod($method, $params)
+    {
+        $reflection = new ReflectionMethod($this->exposed_instance, $method);
+        return $reflection->invokeArgs($this->exposed_instance, $params);
+    }
+
+    // process json-rpc request
+    public function process()
+    {
+        // read input and create request object
+        $json = file_get_contents('php://input');
+        if ($json === false) {
+            throw new \Exception("Server unable to read request body.");
+        }
+        $request = new Request($json);
+
+        // handle json parse error
+        if ($request->error_code && $request->error_message) {
+            echo $request->toResponseJSON();
+        }
+
+        // respond with json
+        echo $this->handleRequest($request);
+    }
+
+    // handle request object / return response json
+    public function handleRequest($request)
+    {
+        // recursion for batch
+        if ($request->isBatch()) {
+            $batch = array();
+            foreach ($request->requests as $req) {
+                $batch[] = $this->handleRequest($req);
+            }
+            return json_encode(array_filter($batch, function($a){return $a === null;}));
+        }
+
+        // check validity of request
+        if ($request->checkValid()) {
+            // check for method existence
+            if (!$this->methodExists($request->method)) {
+                return $request->toErrorResponse(ERROR_METHOD_NOT_FOUND, "Method not found.");
+            }
+
+            // try to call method with params
+            try {
+                $response = $this->invokeMethod($request->method, $request->params);
+                if (!$request->isNotify()) {
+                    $request->result = $response;
+                } else {
+                    return null;
+                }
+            // handle exceptions
+            } catch (Exception $e) {
+                $request->error_code = $e->getCode();
+                $request->error_message = $e->getMessage();
+            }
+        }
+
+        // return whatever we got
+        return $request->toResponseJSON();
+    }
+
+}
 
 ?>
