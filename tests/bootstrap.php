2155238e392e3f6788b082f84bb5c178aa0be7bf
Scott Reis <scott.reis@vertive.com>
2011-08-16 17:37:41 -0500

    fix testing config and write a few tests to get things started

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,7 @@
 <?php
 ini_set('display_errors',1);
-error_reporting(E_ALL);
-
+error_reporting(E_ALL ^ E_NOTICE);
 require_once 'PHPUnit/Framework.php';
-
 require_once('../client.php');
 require_once('../server.php');
 ?>
--- tests/client/clientTest.php
+++ tests/client/clientTest.php
@@ -1,7 +1,95 @@
 <?php
-class ClientTest extends PHPUnit_Framework_TestCase {
+class ClientTest extends \PHPUnit_Framework_TestCase {
 
-    //TODO tests!
+    public function getEmptyRequest()
+    {
+        return $this->getMock('\Junior\Client\Request',
+                              array(),
+                              array(),
+                              '',
+                              false,
+                              false,
+                              false);
+    }
+
+    public function getEmptyResponse()
+    {
+        return $this->getMock('\Junior\Client\Response',
+                              array(),
+                              array(),
+                              '',
+                              false,
+                              false,
+                              false);
+    }
+
+    public function getMockClient(Array $methods = array())
+    {
+        return $this->getMock('\Junior\Client\Client',
+                                 $methods,
+                                 array('foo'));
+    }
+
+    public function testSendRequestGoodId()
+    {
+        $request = $this->getEmptyRequest();
+        $request->id = 10;
+
+        $response = $this->getEmptyResponse();
+        $response->id = 10;
+
+        $client = $this->getMockClient(array('send'));
+        $client->expects($this->once())
+               ->method('send')
+               ->will($this->returnValue($response));
+
+        $this->assertEquals($response, $client->sendRequest($request));
+    }
+
+    public function testSendRequestBadId()
+    {
+        $request = $this->getEmptyRequest();
+        $request->id = 10;
+
+        $response = $this->getEmptyResponse();
+        $response->id = 11;
+
+        $client = $this->getMockClient(array('send'));
+        $client->expects($this->once())
+               ->method('send')
+               ->will($this->returnValue($response));
+
+        $this->setExpectedException('Junior\Client\Exception');
+
+        $client->sendRequest($request);
+    }
+
+    public function testSendNotifyGood()
+    {
+        $request = $this->getEmptyRequest();
+        $request->id = null;
+
+        $client = $this->getMockClient(array('send'));
+        $client->expects($this->once())
+               ->method('send')
+               ->will($this->returnValue(true));
+
+        $this->assertTrue($client->sendNotify($request));
+    }
+
+    public function testSendNotifyBad()
+    {
+        $request = $this->getEmptyRequest();
+        $request->id = 10;
+
+        $client = $this->getMockClient(array('send'));
+        $client->expects($this->never())
+               ->method('send');
+
+        $this->setExpectedException('Junior\Client\Exception');
+
+        $client->sendNotify($request);
+    }
 
 }
 
