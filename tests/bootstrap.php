ce7d80d6376ec02a8c4690d5fc70d93c2d753a18
Scott Reis <scott.reis@vertive.com>
2012-02-06 14:35:02 -0600

    add Spray as a submodule and replace slashes with dir separator constant

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,6 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL ^ E_NOTICE);
 define('ENV', 'TEST');
-require_once '../../spray/src/Spray.php'; //TODO make a submodule of Spray
-require_once 'PHPUnit/Autoload.php';
-require_once '../lib/Junior.php';
+require_once 'Spray' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Spray.php';
+require_once 'PHPUnit/' . DIRECTORY_SEPARATOR . 'Autoload.php';
+require_once '..' . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'Junior.php';
