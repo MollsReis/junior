65b76c0ac87ff54d31a0404b610eaefad31095ac
Konrad Kleine <konrad.kleine@gonicus.de>
2012-07-10 08:29:41 +0200

    Implemented HTTP Basic and HTTP Digest authentication in sublasses of Junior\Client. Due to their dependency on curl, those classes will only be loaded if the curl_init() function exits.

--- lib/Junior.php
+++ lib/Junior.php
@@ -2,3 +2,7 @@
 set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__);
 require_once('Junior' . DIRECTORY_SEPARATOR . 'Client.php');
 require_once('Junior' . DIRECTORY_SEPARATOR . 'Server.php');
+if (function_exists('curl_init')) {
+    require_once('Junior' . DIRECTORY_SEPARATOR . 'ClientBasicAuth.php');
+    require_once('Junior' . DIRECTORY_SEPARATOR . 'ClientDigestAuth.php');
+}
---
+++ lib/Junior/ClientDigestAuth.php
@@ -0,0 +1,73 @@
+<?php
+namespace Junior;
+use Junior\Clientside\Exception,
+    Junior\Client;
+
+require_once('Junior'. DIRECTORY_SEPARATOR . 'Client.php');
+
+/**
+ * This class basically does the same thing as the \c Junior\Client class except
+ * that it can handle HTTP Digest Authentication.
+ *
+ * @FIXME Remove dependency on curl to support PHP installation that don't have it.
+ * @TODO Add tests for this class
+ *
+ * @author Konrad Kleine (kwk)
+ */
+class ClientDigestAuth extends Client {
+
+    private $username = "";
+    private $password = "";
+    
+    /**
+     * Sets the \a $username and \a $password to be used for the HTTP Digest
+     * authentication and passes the \a $uri to the parent constructor.
+     */
+    public function __construct($uri, $username, $password)
+    {
+        parent::__construct($uri);
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    /**
+     * @override
+     */
+    public function send($json, $notify = false)
+    {
+        $response = false;
+        
+        $ch = curl_init($this->uri);
+        // Return result as string instead of printing it
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // HTTP Post
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+        // Follow any "Location: " header
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        // Keep sending the username and password when following locations
+        curl_setopt($ch, CURLOPT_UNRESTRICTED_AUTH, true);
+        // Apply the curl authentication options
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_DIGEST);
+        curl_setopt($ch, CURLOPT_USERPWD, "{$this->curlUsername}:{$this->curlPassword}");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        // handle communication errors
+        if ($response === false) {
+            throw new Clientside\Exception("Unable to connect to {$this->uri}");
+        }
+
+        // notify has no response
+        if ($notify) {
+            return true;
+        }
+
+        // try to decode json
+        $json_response = $this->decodeJSON($response);
+
+        // handle response, create response object and return it
+        return $this->handleResponse($json_response);
+    }
+}
